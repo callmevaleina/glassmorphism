8d5b99d0cbef802d9df8dfd0166aba4757a2701b
callmevaleina <valentina.crms@gmail.com>
2022-08-15 00:42:49 -0400

    last changes

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ console.log(opacity);
 const blur = document.querySelector('#blur');
 console.log(blur);
 
+const saturation = document.querySelector('#saturation');
+console.log(saturation);
+
+const contrast = document.querySelector('#contrast');
+console.log(contrast);
+
 const size = document.querySelector('#size');
 console.log(size);
 
@@ -20,17 +26,33 @@ console.log(radius);
 
 const valueOpacity = document.querySelector('.value-opacity');
 
-const valueBlur = document.querySelector('.value-blur')
+const valueBlur = document.querySelector('.value-blur');
 
-const valueSize = document.querySelector('.value-size')
+const valueSaturation = document.querySelector('.value-saturation');
 
-const valueRadius = document.querySelector('.value-radius')
+const valueContrast = document.querySelector('.value-contrast');
+
+const valueSize = document.querySelector('.value-size');
+
+const valueRadius = document.querySelector('.value-radius');
+
+const cssCodeContainer = document.querySelector('.css-code-container')
 
 
 
 let rgb;
 
-function hexToRgbA(hex){
+let valuesCode = [];
+let radiusCode;
+console.log(radiusCode);
+let backgroundCode;
+let backdropFilterBlur;
+let backdropFilterSaturation;
+let backdropFilterContrast;
+
+
+
+function hexToRgb(hex){
     var c;
     if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
     c= hex.substring(1).split('');
@@ -44,10 +66,22 @@ function hexToRgbA(hex){
 }
 
 color.addEventListener('input', (e)=>{
-    rgb = hexToRgbA(e.target.value)
+    rgb = hexToRgb(e.target.value)
     let rgba = `rgba(${rgb}, ${opacity.value})`
     console.log(rgba);
     glassElement.style.background = rgba
+    backgroundCode = rgba
+    valuesCode.push(
+        `<div class="css-code" style="background: black;" style="color: white;" width="450px" height="250px" style="font-family:'Lato';" style="font-size: 12px;">
+            <p>.glass {</p>
+            <p> border-radius: ${radiusCode} px</p>
+            <p> background: ${backgroundCode}</p>
+            <p> backdrop-filter: blur(${backdropFilterBlur} px) saturate(${backdropFilterSaturation}%) contrast(${backdropFilterContrast}%)</p>     
+            <p>}</p>
+        </div>`
+      );
+    const valuesCodeText = valuesCode.join("");
+    cssCodeContainer.innerHTML = valuesCodeText;
     
 });
 
@@ -58,27 +92,44 @@ opacity.addEventListener('input', (e) =>{
     glassElement.style.background = rgba
     console.log(rgba);    
     valueOpacity.value = `${e.target.value}%`
+    console.log(e.target.value);
 
 });
 
 blur.addEventListener('input', (e)=>{
     glassElement.style.backdropFilter = `blur(${e.target.value}px)`
-    glassElement.style.webkit.backdropFilter = `blur(${e.target.value}px)`
+    glassElement.style.webkitBackdropFilter = `blur(${e.target.value}px)`
     valueBlur.value = `${e.target.value}px`
+    backdropFilterBlur = e.target.value
+    
 }); 
+
+saturation.addEventListener('input', (e)=>{
+    glassElement.style.backdropFilter = `saturate(${e.target.value}%)`
+    glassElement.style.webkitBackdropFilter = `saturate(${e.target.value}%)`
+    valueSaturation.value = `${e.target.value}%`
+    backdropFilterSaturation = e.target.value
+}); 
+
+contrast.addEventListener('input', (e)=>{
+    glassElement.style.backdropFilter = `contrast(${e.target.value}%)`
+    glassElement.style.webkitBackdropFilter = `contrast(${e.target.value}%)`
+    valueContrast.value = `${e.target.value}%`
+    backdropFilterContrast = e.target.value
+}); 
+
 
 size.addEventListener('input', (e)=>{
     glassElement.style.width = `${e.target.value}px`
     glassElement.style.height = `${e.target.value}px`
-    valueSize.value = `${e.target.value}px`
-    console.log(e.target.value);
+    valueSize.value = e.target.value
 }); 
 
 radius.addEventListener('input', (e)=>{
     glassElement.style.borderRadius = `${e.target.value}px`
     valueRadius.value = `${e.target.value}px`
+    radiusCode = e.target.value
 }); 
-
 
 
 
